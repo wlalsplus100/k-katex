0c784474ec6aa0684c442645350f10b4c8724963
wlals <jiminelp@gmail.com>
2024-06-04 12:01:41 +0900

    FEAT :: 메인 컴포넌트 구현

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
+import { TableComponent } from '../table/table.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [TableComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css',
 })
 export class MainComponent {
   title = 'K-KaTeX';
   search = '';
-  onKey(event: any) {
+  labels: string[] = [
+    'K-KaTeX에서의 모든 KaTeX 문법에 대한 정리는 다음과 같이 이루어집니다',
+  ];
+  tableItem = 'main';
+  onKeyUp(event: any) {
     this.search = event.target.value;
   }
 }
